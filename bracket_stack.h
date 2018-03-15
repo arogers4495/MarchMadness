22f34bd3e9f5c9732cb9e18ad2fda60e2621f81c
Fred Bunt <fredrick.bunt@gmail.com>
2018-03-15 15:21:34 -0600

    Add doc strings

--- bracket_stack.h
+++ bracket_stack.h
@@ -4,26 +4,49 @@
 #include <stdbool.h>
 #include "team.h"
 
+
+// Stack node for internal use.
 typedef struct stack_node_struct
 {
     team* t;
     struct stack_node_struct* prev;
 } stack_node;
 
+
+/**
+ * A stack for storing teams in the NCAA March Madness bracket.
+ */
 typedef struct bracket_stack_struct {
     stack_node* head;
     int size;
 } bracket_stack;
 
 
+/**
+ * Initialize the stack to a valid empty state.
+ */
 void initialize(bracket_stack* s);
 
+/**
+ * Add a team to the stack.
+ */
 void push(bracket_stack* s, char* name, int seed);
 
+/**
+ * Return and remove the top element in the stack using the supplied team
+ * pointer
+ */
 void pop(bracket_stack* s, team* t);
 
+/**
+ * Return without removing the top element in the stack using the supplied
+ * team pointer.
+ */
 void peek(bracket_stack* s, team* t);
 
+/**
+ * Returns true if the stack has no elements.
+ */
 bool is_empty(bracket_stack* s);
 
 /**
