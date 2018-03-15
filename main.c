73d5fd025133c3c305c74f367ff85e7445d8a3cd
Fred Bunt <fredrick.bunt@gmail.com>
2018-03-15 13:36:44 -0600

    Make minor changes to main.c

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main() {
     //check to see if null
     if (myFile == NULL) {
         printf("Could not open file.\n");
-    } else {
-        //iterate through the text & collect team name/seed
-        for (i = 0; i < 68; ++i) {
-            //create temp variables for the scan here
-            char teamName[20];
-            int teamSeed;
-            fscanf(myFile, "%s,%d", teamName, &teamSeed);
-        }
-        return 0;
-
+        return 1;
     }
+
+    //iterate through the text & collect team name/seed
+    for (i = 0; i < 68; ++i) {
+        //create temp variables for the scan here
+        char teamName[50];
+        int teamSeed;
+        fscanf(myFile, "%s,%d", teamName, &teamSeed);
+    }
+    return 0;
 }
